6763027870058bd42549a9121b9a9df866347ff2
Adam Bogdał <adam@bogdal.pl>
2015-03-03 15:05:49 +0100

    Bump version and add classifiers

--- setup.py
+++ setup.py
@@ -8,13 +8,27 @@ setup(
     author_email='hello@mirumee.com',
     description="Cloud static and media file storage suitable for app containers",
     license='MIT',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/mirumee/django-offsite-storage',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'boto>=2.36.0',
         'Django>=1.7',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     zip_safe=False
 )
